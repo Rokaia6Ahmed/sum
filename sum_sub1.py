a8da6fd67e6ec6ac86d2eca9857fdf1f3e48033b
Rokaia6Ahmed <153697971+Rokaia6Ahmed@users.noreply.github.com>
2023-12-23 12:19:38 +0200

    sum_sub1

--- sum_sub1.py
+++ sum_sub1.py
@@ -2,10 +2,11 @@ def sum(num1,num2):
     return num1+num2
 
 
-
-    
+def sub(num_1,num_2):
+    sub=num_1-num_2
+    print(sub)
 
 number1= int (input("enter number1:"))
 number2= int (input("enter number2:"))
 print(sum(number1,number2))
-
+sub(number1,number2)
